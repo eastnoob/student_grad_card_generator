e2f334149d7e640484de78c6c833af2dae1da88b
Eastnoob <61303573+eastnoob@users.noreply.github.com>
2023-01-12 00:06:46 +0800

    Initial commit

---
+++ comment.py
@@ -0,0 +1,34 @@
+class Comment():
+
+    def __init__(self, name: str, 
+                    comment: str, 
+                    teacher: str) -> str:
+        
+        self.name = name
+        self.comment = comment
+        self.teacher = teacher
+        # self.time = time
+        self.result = self.joinComment()
+        
+        # return joint
+        
+    def getTime(self):
+        year = input("请输入年：")
+        month = input("请输入月：")
+        day = input("请输入日：")
+        
+        time = year + "年" + month + "月" + day + "日"
+        
+        return time
+
+
+    def joinComment(self) -> str:
+        time = self.getTime()
+        
+        joint_result = ""
+        joint_result += self.name + ": \n" + "  " + self.comment + self.teacher + "\n" + "<p align=\"right\">" + time + "</p>"
+
+        return joint_result
+
+
+
---
+++ main.py
@@ -0,0 +1,18 @@
+# 1. 打开成绩单
+# 2. 提取人名，成绩
+# 3. 将人名、成绩写入一页
+
+# 1. 打开评语
+# 2. 将人名，评语写在第二页
+
+# a = input("ccc")
+# print(type(a))
+
+# print("test")
+import comment
+
+a = comment.Comment("1", "2", "3")
+
+print(a.result)
+
+# print(a.comment_joint())
