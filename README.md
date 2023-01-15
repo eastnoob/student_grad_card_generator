ffe1747a970031d8b4b327ceedbba704d514f43d
Eastnoob <61303573+eastnoob@users.noreply.github.com>
2023-01-15 10:22:51 +0800

    添加说明

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # student_grad_card_generator
  尝试做一个可以自动生成学生期末成绩卡片的脚本
 正在做
+
+![效果图](README_img/3_%E8%93%9D%E6%A2%A6_00.png "效果图")
--- test.js
+++
@@ -1,8 +0,0 @@
-// const command = 'markdown.marp.export';
-// const command = 'workbench.action.openRawDefaultSettings';
-
-// vscode.commands.executeCommand(command);
-
-vscode.commands.executeCommand('workbench.action.openRawDefaultSettings').then(result => {
-    console.log(result);
-});
